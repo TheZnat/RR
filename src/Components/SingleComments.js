820f184d01008890b7fd62bb49d46c1a043a6d4f
Max K <TheZnat7@yandex.ru>
2024-08-19 03:10:32 +0300

    end project

--- src/Components/SingleComments.js
+++ src/Components/SingleComments.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { commentDelete, commentUpdate } from "../redux/action";
 
 const SingleComments = ({ data }) => {
   const [commentText, setCommentText] = useState("");
   const { text, id } = data;
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (text) {
@@ -14,9 +17,21 @@ const SingleComments = ({ data }) => {
     setCommentText(e.target.value);
   };
 
+  const handlerUpdate = (e) => {
+    e.preventDefault();
+    dispatch(commentUpdate(commentText, id));
+  };
+
+  const handlerDelete = (e) => {
+    e.preventDefault();
+    dispatch(commentDelete(id));
+  };
+
   return (
-    <form className="comments-item">
-      <div className="comments-item-delete">&times;</div>
+    <form className="comments-item" onSubmit={handlerUpdate}>
+      <div className="comments-item-delete" onClick={handlerDelete}>
+        &times;
+      </div>
       <input type="text" value={commentText} onChange={handlerInput} />
       <input type="submit" hidden />
     </form>
--- src/redux/action.js
+++ src/redux/action.js
@@ -1,30 +1,103 @@
-import { COMMENT_CREATE, DECREMENT, INCREMENT, INPUT_TEXT } from "./type";
+import {
+  COMMENT_CREATE,
+  COMMENT_DELETE,
+  COMMENT_UPDATE,
+  COMMENTS_LOAD,
+  DECREMENT,
+  ERROR_DISPLAY_OFF,
+  ERROR_DISPLAY_ON,
+  INCREMENT,
+  INPUT_TEXT,
+  LOADER_DISPLAY_OFF,
+  LOADER_DISPLAY_ON,
+} from "./type";
 
-
-
-export function incrementLikes(){
-    return{
-        type: INCREMENT
-    }
+export function incrementLikes() {
+  return {
+    type: INCREMENT,
+  };
 }
 
-export function decrementLikes(){
-    return{
-        type: DECREMENT
-    }
+export function decrementLikes() {
+  return {
+    type: DECREMENT,
+  };
 }
 
-export function inputText(text){
-    return{
-        type: INPUT_TEXT,
-        text
-    }
+export function inputText(text) {
+  return {
+    type: INPUT_TEXT,
+    text,
+  };
 }
 
-export function commentCreate(text, id){
-    return{
-        type: COMMENT_CREATE,
-        data: {text, id}
-    }
-};
+export function commentCreate(text, id) {
+  return {
+    type: COMMENT_CREATE,
+    data: { text, id },
+  };
+}
 
+export function commentUpdate(text, id) {
+  return {
+    type: COMMENT_UPDATE,
+    data: { text, id },
+  };
+}
+
+export function commentDelete(id) {
+  return {
+    type: COMMENT_DELETE,
+    id,
+  };
+}
+
+export function loaderOn() {
+  return {
+    type: LOADER_DISPLAY_ON,
+  };
+}
+
+export function loaderOff() {
+  return {
+    type: LOADER_DISPLAY_OFF,
+  };
+}
+
+export function errorOn(text) {
+  return (dispatch) => {
+    dispatch({
+      type: ERROR_DISPLAY_ON,
+      text,
+    });
+
+    setTimeout(() => {
+      dispatch(errorOff());
+    }, 3000);
+  };
+}
+
+export function errorOff() {
+  return {
+    type: ERROR_DISPLAY_OFF,
+  };
+}
+
+export function commentsLoad() {
+  return async (dispatch) => {
+    try {
+      dispatch(loaderOn());
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/comments?_limit=10"
+      );
+      const data = await response.json();
+      dispatch({
+        type: COMMENTS_LOAD,
+        data,
+      });
+      dispatch(loaderOff());
+    } catch (err) {
+      dispatch(errorOn(err.message));
+    }
+  };
+}
